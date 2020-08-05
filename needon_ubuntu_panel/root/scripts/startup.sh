59e28110973acbe1561a5f3763b686471630acdf
k4584587 <k4584587@naver.com>
2020-08-05 10:34:40 +0900

    .

---
+++ needon_ubuntu_panel/root/scripts/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "[!] ssh 서비스를 시작합니다."
+service ssh start
+
+echo "[!] docker 서비스를 시작합니다."
+service docker start
---
+++ needon_ubuntu_panel/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#docker run --rm --net=host --name needon_ubuntu k45845871/needon_ubuntu:18.04
+docker run --rm --net=host --name needon_ubuntu k45845871/needon_ubuntu:needon_panel
